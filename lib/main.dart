edd265a423486c4ce932baedd5df47fbcec08a37
rampo <rajeshsankaravadivell@gmail.com>
2022-04-16 12:16:00 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,14 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key, }) : super(key: key);
-
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       home: InputForm(),
     );
